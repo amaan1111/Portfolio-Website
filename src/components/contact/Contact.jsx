dae4372a14762e49292cbf2748ff9ccbf110982a
Amaan Bagalkote <amaanbagalkote@gmail.com>
2022-03-13 15:02:12 +0530

    initial

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,38 +21,57 @@ const Contact = () => {
   };
 
   return (
-    <section id='contact'>
+    <section id="contact">
       <h2>Contact Me</h2>
       <div className="container contact__container">
-        <div className='contact__options'>
-        <article className='contact__option'>
-        <MdOutlineEmail className='contact__option-icon'/>
-          <h4>Email</h4>
-          <h5>amaanbagalkote1@gmail.com</h5>
-          <a href="mailto:amaanbagalkote1@gmail.com" target='_blank'>Send a Message</a>
-        </article>
-        <article className='contact__option'>
-        <BsLinkedin className='contact__option-icon'/>
-          <h4>LinkedIn</h4>
-          <h5>Amaan Bagalkote</h5>
-          <a href="https://www.linkedin.com/in/amaan-bagalkote-684168148/" target='_blank'>Send a Message</a>
-        </article>
-        <article className='contact__option'>
-        <BsWhatsapp className='contact__option-icon'/>
-          <h4>WhatsApp</h4>
-          <h5>+91 9607040104</h5>
-          <a href="https://api.whatsapp.com/send?phone=919607040104" target='_blank'>Send a Message</a>
-        </article>
-      </div>
-      <form ref={form} onSubmit={sendEmail}>
-        <input type="text" name='name' placeholder='Your Name' required / >
-        <input type="email" name='email' placeholder='Your Email' required />
-        <textarea type="message" rows='7' placeholder='Your Message' required></textarea>
-        <button type='submit' className='btn btn-primary'>Send Message</button>
-      </form>
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>amaanbagalkote1@gmail.com</h5>
+            <a href="mailto:amaanbagalkote1@gmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+            <h4>LinkedIn</h4>
+            <h5>Amaan Bagalkote</h5>
+            <a
+              href="https://www.linkedin.com/in/amaan-bagalkote-684168148/"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+91 8830082842</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=918830082842"
+              target="_blank"
+            >
+              Send a Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            type="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
-  )
+  );
 }
 
 export default Contact
