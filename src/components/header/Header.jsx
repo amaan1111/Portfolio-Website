aa04bbbd10161d90c1ddbd003da0a15e5856856c
Amaan Bagalkote <amaanbagalkote@gmail.com>
2022-11-15 13:40:10 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,21 +7,22 @@ import {AiOutlineArrowRight} from 'react-icons/ai'
 const Header = () => {
   return (
     <header>
-        <div className="container header__container">
-            <h5>Hello I'm </h5>
-            <h1>Amaan</h1>
-            <h5 className="text-light">
-                Full Stack Developer
-            </h5>
-            <CTA/>
-            <HeaderSocials/>
-            <div >
-                <img src={ME} alt='me' className="me"/>
-            </div>
-            <a href="#contact" className='scroll__down'>Scroll Down<AiOutlineArrowRight/></a>
+      <div className="container header__container">
+        <h5>Hello I'm </h5>
+        <h1>Amaan</h1>
+        <h5 className="text-light">Front End Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <div>
+          <img src={ME} alt="me" className="me" />
         </div>
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+          <AiOutlineArrowRight />
+        </a>
+      </div>
     </header>
-  )
+  );
 }
 
 export default Header
